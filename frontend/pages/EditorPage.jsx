18cc83818b7fefc193d6ed50ef24023e0506a858
hafi-ansari <hafi.m.ansari@gmail.com>
2023-06-13 17:50:26 -0500

    Python syntax code editor + submit button

--- frontend/pages/EditorPage.jsx
+++ frontend/pages/EditorPage.jsx
@@ -6,6 +6,7 @@ import 'split-pane-react/esm/themes/default.css';
 const EditorPage = () => {
   const [sizes, setSizes] = useState(['40%', '60%']);
   const [innerSizes, setInnerSizes] = useState(['70%', '30%']);
+  const [code, setCode] = useState('Enter code here...')
 
   const stopPropagation = (e) => {
     e.stopPropagation();
@@ -22,6 +23,11 @@ const EditorPage = () => {
       window.removeEventListener('dragend', stopPropagation);
     };
   }, []);
+
+  const codeSubmit = () =>{
+    console.log(code)
+  }
+
 
   return (
     <div className="App h-screen dark:bg-dark-primary">
@@ -47,13 +53,13 @@ const EditorPage = () => {
             onDragEnd={stopPropagation}
           >
             <Pane>
-              <Editor className="border-b-4 border-black"/> 
+              <Editor className="border-b-4 border-black" onCodeChange={setCode}/> 
             </Pane>
             <Pane minSize="20%" maxSize="80%">
               <div className="h-full bg-dark-secondary p-6 border-t-4 border-black">
                 <div className="flex justify-between items-center">
-                  <h2 className="text-white">Console</h2>
-                  <button className="px-4 py-2 rounded text-white">Submit</button>
+                  <h2 className="mb-4 text-white font-bold">Console</h2>
+                  <button className="px-2 py-2 rounded text-white bg-dark-accent" onClick={codeSubmit}>Submit</button>
                 </div>
               </div>
             </Pane>

688a5e705f771f993398d36c5dcf47ac3b6e2aff
hafi-ansari <hafi.m.ansari@gmail.com>
2023-06-15 15:00:31 -0500

    Handled for testcase validation and button color change

--- frontend/components/EditorPageComponents/TestCaseSelector.jsx
+++ frontend/components/EditorPageComponents/TestCaseSelector.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import CaseButton from "./CaseButton";
 import TestCase from "./TestCase";
 
-const TestCaseSelector = ({ activeCase, onSelectCase, results, testCases }) => (
+const TestCaseSelector = ({ activeCase, onSelectCase, results, testCases, isCorrect }) => (
   <>
     {testCases.map((testCase, index) => (
-      <CaseButton key={index} caseNumber={index + 1} onSelectCase={() => onSelectCase(index + 1)} />
+      <CaseButton key={index} caseNumber={index + 1} onSelectCase={() => onSelectCase(index + 1)} isCorrect={isCorrect[index]} />
     ))}
 
     {testCases.map((testCase, index) => {
